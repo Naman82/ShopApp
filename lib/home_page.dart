c75665219c01e4ea931ed99638ae36fcd1d36f25
Naman82 <b520031@iiit-bh.ac.in>
2023-10-26 19:22:53 +0530

    filter banner added

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> filters = const ['All', 'Addidas', 'Nike', 'Bata'];
+  late String currFilter;
+
+  @override
+  void initState() {
+    super.initState();
+    currFilter = filters[0];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,11 +27,11 @@ class HomePage extends StatelessWidget {
         left: Radius.circular(50),
       ),
     );
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Column(
           children: [
-            Row(
+            const Row(
               children: [
                 Padding(
                   padding: EdgeInsets.all(20.0),
@@ -39,6 +53,43 @@ class HomePage extends StatelessWidget {
                 )
               ],
             ),
+            SizedBox(
+              height: 120,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: filters.length,
+                  itemBuilder: (context, index) {
+                    final filter = filters[index];
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            currFilter = filter;
+                          });
+                        },
+                        child: Chip(
+                          backgroundColor: currFilter == filter
+                              ? Theme.of(context).colorScheme.primary
+                              : const Color.fromRGBO(245, 247, 249, 1),
+                          side: const BorderSide(
+                            color: Color.fromRGBO(245, 247, 249, 1),
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          label: Text(filter),
+                          labelStyle: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 14, vertical: 10),
+                        ),
+                      ),
+                    );
+                  }),
+            )
           ],
         ),
       ),
